e6ca2731c479fe399f9a2dfc170c0a30828c3091
cp06523 <69167973+cp06523@users.noreply.github.com>
2025-02-23 19:52:11 -0500

    Create preprocess.py

---
+++ ball-action-spotting/inference_preprocessing/preprocess.py
@@ -0,0 +1,18 @@
+import cv2
+from src.ball_action import constants
+
+def preprocess_video(video_path : Path):
+  cap = cv2.VideoCapture(video_path)
+  fps = cv2.get(cv2.CAP_PROP_FPS)
+  output_file = 'output.mp4'
+  output_fps = 25
+  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+  out = cv2.VideoWriter(output_file, fourcc, output_fps, (int(cap.get(3)), int(cap.get(4))))
+  while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    out.write(frame)
+    cap.release()
+out.release()
+cv2.destroyAllWindows()
