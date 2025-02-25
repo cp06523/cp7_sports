a3e38e38d0433b74dc2ebd65d40b8bded53f06c6
cp06523 <69167973+cp06523@users.noreply.github.com>
2025-02-24 19:36:02 -0500

    Update preprocess.py

--- ball-action-spotting/inference_preprocessing/preprocess.py
+++ ball-action-spotting/inference_preprocessing/preprocess.py
@@ -4,7 +4,7 @@ from src.ball_action import constants
 def preprocess_video(video_path : Path):
   cap = cv2.VideoCapture(video_path)
   fps = cv2.get(cv2.CAP_PROP_FPS)
-  output_file = 'output.mp4'
+  output_file = constants.inference_dir \ 'output.mp4'
   output_fps = 25
   fourcc = cv2.VideoWriter_fourcc(*'mp4v')
   out = cv2.VideoWriter(output_file, fourcc, output_fps, (int(cap.get(3)), int(cap.get(4))))
