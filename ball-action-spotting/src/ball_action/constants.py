dac7cc2aeaca929a1c5c256da00f11bf16a52c4a
cp06523 <69167973+cp06523@users.noreply.github.com>
2025-02-25 22:35:22 -0500

    Update constants.py

--- ball-action-spotting/src/ball_action/constants.py
+++ ball-action-spotting/src/ball_action/constants.py
@@ -9,29 +9,10 @@ visualizations_dir = ball_action_dir / "visualizations"
 model_path = '/content/drive/MyDrive/SoccerNet/ball-action-spotting/data/ball_action (1)/experiments/sampling_weights_001/fold_0/model-029-0.834085.pth'
 soccernet_dir = soccernet_dir / "spotting-ball-2024"
 
-fold_games = [
-    "england_efl/2019-2020/2019-10-01 - Leeds United - West Bromwich",
-    "england_efl/2019-2020/2019-10-01 - Hull City - Sheffield Wednesday",
-    "england_efl/2019-2020/2019-10-01 - Brentford - Bristol City",
-    "england_efl/2019-2020/2019-10-01 - Blackburn Rovers - Nottingham Forest",
-    "england_efl/2019-2020/2019-10-01 - Middlesbrough - Preston North End",
-    "england_efl/2019-2020/2019-10-01 - Stoke City - Huddersfield Town",
-    "england_efl/2019-2020/2019-10-01 - Reading - Fulham",
-]
-fold2games = {fold: [game] for fold, game in enumerate(fold_games)}
-game2fold = {game: fold for fold, games in fold2games.items() for game in games}
-folds = sorted(fold2games.keys())
-challenge_games = [
-    "england_efl/2019-2020/2019-10-02 - Cardiff City - Queens Park Rangers",
-    "england_efl/2019-2020/2019-10-01 - Wigan Athletic - Birmingham City",
-]
-
-classes = [
-    "PASS",
-    "DRIVE",
-]
 labels_filename = "Labels-ball.json"
+
 videos_extension = 'mp4'
+
 classes = ['PASS', 'DRIVE', 'HEADER', 'HIGH PASS', 'OUT', 'CROSS', 'THROW IN',
            'SHOT', 'BALL PLAYER BLOCK', 'PLAYER SUCCESSFUL TACKLE', 'FREE KICK',
            'GOAL']
@@ -42,6 +23,7 @@ class2target: dict[str, int] = {cls: trg for trg, cls in enumerate(classes)}
 
 num_halves = 2
 halves = list(range(1, num_halves + 1))
+
 postprocess_params = {
     "gauss_sigma": 3.0,
     "height": 0.2,
